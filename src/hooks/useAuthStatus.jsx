d3976235e46a13dde77dc7a3bf55df5a2d46a157
Ilmaooo <ilmaogresevic1@gmail.com>
2023-01-04 10:06:04 +0100

    private route and custom hook created for protecting the profile page and logout functionality added

---
+++ src/hooks/useAuthStatus.jsx
@@ -0,0 +1,23 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+export default function useAuthStatus() {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [checkingStatus, setChekingStatus] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setLoggedIn(true);
+      }
+      setChekingStatus(false);
+    });
+  }, []);
+  return {
+    loggedIn,
+    checkingStatus,
+  };
+}
