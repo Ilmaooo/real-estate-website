8a7e61355d893b433d9534a671c8762a4e2d4ffe
Ilmaooo <ilmaogresevic1@gmail.com>
2023-01-05 17:24:14 +0100

    share link added

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,7 +1,7 @@
 import { doc, getDoc } from "firebase/firestore";
 import { useState } from "react";
 import { useEffect } from "react";
-import { useParams } from "react-router";
+import { useParams } from "react-router-dom";
 import Spinner from "../components/Spinner";
 import { db } from "../Firebase";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -12,11 +12,13 @@ import SwiperCore, {
   Pagination,
 } from "swiper";
 import "swiper/css/bundle";
+import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
   const params = useParams();
   const [listing, setListing] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
   SwiperCore.use([Autoplay, Navigation, Pagination]);
   useEffect(() => {
     async function fetchListing() {
@@ -40,13 +42,13 @@ export default function Listing() {
         navigation
         pagination={{ type: "progressbar" }}
         effect="fade"
-        module={[EffectFade]}
+        modules={[EffectFade]}
         autoplay={{ delay: 3000 }}
       >
         {listing.imgUrls.map((url, index) => (
           <SwiperSlide key={index}>
             <div
-              className=" relative overflow-hidden h-[400px] w-full"
+              className=" relative overflow-hidden h-[500px] w-full"
               style={{
                 background: `url(${listing.imgUrls[index]}) center no-repeat`,
                 backgroundSize: "cover",
@@ -55,6 +57,23 @@ export default function Listing() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div
+        onClick={() => {
+          navigator.clipboard.writeText(window.location.href);
+          setShareLinkCopied(true);
+          setTimeout(() => {
+            setShareLinkCopied(false);
+          }, 2000);
+        }}
+        className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer rounded-full border-2 border-gray-400 w-12 h-12 flex justify-center items-center"
+      >
+        <FaShare className="text-lg font-light text-slate-500" />
+      </div>
+      {shareLinkCopied && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-1 text-slate-600">
+          Link Copied!
+        </p>
+      )}
     </main>
   );
 }
